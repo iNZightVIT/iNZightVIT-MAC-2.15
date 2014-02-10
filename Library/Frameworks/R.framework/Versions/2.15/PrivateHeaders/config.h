6336a515f4b8081819b7a87fbfc619f4be5ada21
Tom Elliott <tomelliott90@gmail.com>
2014-02-10 13:12:57 +1300

    added R 2.15 and required libraries

---
+++ Library/Frameworks/R.framework/Versions/2.15/PrivateHeaders/config.h
@@ -0,0 +1,14 @@
+/* This is an automatically generated universal stub for architecture-dependent headers. */
+#ifdef __i386__
+#include "i386/config.h"
+#elif defined __ppc__
+#include "ppc/config.h"
+#elif defined __ppc64__
+#include "ppc64/config.h"
+#elif defined __x86_64__
+#include "x86_64/config.h"
+#elif defined __arm__
+#include "arm/config.h"
+#else
+#error "Unsupported architecture."
+#endif
---
+++ Library/Frameworks/R.framework/Versions/2.15/Resources/include/x86_64/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 134915
+#define R_NICK "Security Blanket"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "2"
+#define R_MINOR  "15.3"
+#define R_STATUS ""
+#define R_YEAR   "2013"
+#define R_MONTH  "03"
+#define R_DAY    "01"
+#define R_SVN_REVISION "62090"
+#define R_FILEVERSION    2,153,62090,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
